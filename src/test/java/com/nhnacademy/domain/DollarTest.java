be2c301e1f2350ad0b552e82a76a7595af53d5a4
윤동열 <eastheat10@gmail.com>
2022-05-11 19:09:08 +0900

    chapter5. 프라이버시

--- src/test/java/com/nhnacademy/domain/DollarTest.java
+++ src/test/java/com/nhnacademy/domain/DollarTest.java
@@ -8,14 +8,14 @@ import org.junit.jupiter.api.Test;
 class DollarTest {
 
     @DisplayName("달러 객체 연산 후 값이 바뀌는 문제 해결")
-    @Test
+//    @Test
     void testMultiplication() {
-        Dollar five = new Dollar(5);
-        Dollar product = five.times(2);
-        assertThat(10).isEqualTo(product);
-
-        product = five.times(3);
-        assertThat(15).isEqualTo(product);
+//        Dollar five = new Dollar(5);
+//        Dollar product = five.times(2);
+//        assertThat(10).isEqualTo(product);
+//
+//        product = five.times(3);
+//        assertThat(15).isEqualTo(product);
     }
 
     @DisplayName("동등성")
@@ -29,5 +29,24 @@ class DollarTest {
     void testEquality2() {
         assertThat(new Dollar(5).equals(new Dollar(5))).isTrue();
         assertThat(new Dollar(6).equals(new Dollar(5))).isFalse();
+    }
+
+    @Test
+    @DisplayName("private 만들기")
+    void testMultiplication_private() {
+        Dollar five = new Dollar(5);
+        Dollar product = five.times(2);
+        assertThat(new Dollar(10)).isEqualTo(product);
+
+        product = five.times(3);
+        assertThat(new Dollar(15)).isEqualTo(product);
+    }
+
+    @Test
+    @DisplayName("테스트 코드 리팩토링")
+    void testMultiplication_refactor() {
+        Dollar five = new Dollar(5);
+        assertThat(new Dollar(10)).isEqualTo(five.times(2));
+        assertThat(new Dollar(15)).isEqualTo(five.times(3));
     }
 }
