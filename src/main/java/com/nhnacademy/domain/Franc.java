381cff0289e4f75043c00048dc77a9030902b016
윤동열 <eastheat10@gmail.com>
2022-06-07 17:11:44 +0900

    chapter11. 모든 악의 근원

--- src/main/java/com/nhnacademy/domain/Franc.java
+++
@@ -1,8 +0,0 @@
-package com.nhnacademy.domain;
-
-public class Franc extends Money {
-
-    public Franc(int amount, String currency) {
-        super(amount, currency);
-    }
-}
--- src/test/java/com/nhnacademy/domain/DollarTest.java
+++ src/test/java/com/nhnacademy/domain/DollarTest.java
@@ -7,62 +7,11 @@ import org.junit.jupiter.api.Test;
 
 class DollarTest {
 
-    @DisplayName("달러 객체 연산 후 값이 바뀌는 문제 해결")
-//    @Test
-    void testMultiplication() {
-//        Dollar five = new Dollar(5);
-//        Dollar product = five.times(2);
-//        assertThat(10).isEqualTo(product);
-//
-//        product = five.times(3);
-//        assertThat(15).isEqualTo(product);
-    }
-
+    @Test
     @DisplayName("동등성")
-    @Test
-    void testEquality1() {
-        assertThat(new Dollar(5, "USD").equals(new Dollar(5, "USD"))).isTrue();
-    }
-
-    @DisplayName("동등성")
-    @Test
-    void testEquality2() {
-        assertThat(new Dollar(5, "USD").equals(new Dollar(5, "USD"))).isTrue();
-        assertThat(new Dollar(6, "USD").equals(new Dollar(5, "USD"))).isFalse();
-    }
-
-    @Test
-    @DisplayName("private 만들기")
-    void testMultiplication_private() {
-        Dollar five = new Dollar(5, "USD");
-        Money product = five.times(2);
-        assertThat(new Dollar(10, "USD")).isEqualTo(product);
-
-        product = five.times(3);
-        assertThat(new Dollar(15, "USD")).isEqualTo(product);
-    }
-
-    @Test
-    @DisplayName("테스트 코드 리팩토링")
-    void testMultiplication_refactor() {
-        Money five = Money.dollar(5);
-        assertThat(Money.dollar(10)).isEqualTo(five.times(2));
-        assertThat(Money.dollar(15)).isEqualTo(five.times(3));
-    }
-
-    @Test
-    public void tesFrancMultiplication() {
-        Franc five = Money.franc(5);
-        assertThat(Money.franc(10)).isEqualTo(five.times(2));
-    }
-
-    @Test
-    @DisplayName("동등성2")
     void testEquality() {
         assertThat(Money.dollar(5).equals(Money.dollar(5))).isTrue();
         assertThat(Money.dollar(5).equals(Money.dollar(6))).isFalse();
-        assertThat(new Franc(5, "CHF").equals(Money.franc(5))).isTrue();
-        assertThat(new Franc(5, "CHF").equals(Money.franc(6))).isFalse();
         assertThat(Money.dollar(5).equals(Money.franc(5))).isFalse();
     }
 
@@ -71,10 +20,5 @@ class DollarTest {
     void testCurrency() {
         assertThat("USD").isEqualTo( Money.dollar(1).currency());
         assertThat("CHF").isEqualTo( Money.franc(1).currency());
-    }
-
-    @Test
-    void testDifferentClassEquality() {
-        assertThat(new Money(10, "CHF")).isEqualTo(new Franc(10, "CHF"));
     }
 }
